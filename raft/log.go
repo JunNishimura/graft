3761eda601b353d0c3d5068a1aecfbd30ac785d0
Jun Nishimura <n.junjun0303@gmail.com>
2025-06-15 15:37:32 +0900

    feat: type definition for log

---
+++ raft/log.go
@@ -0,0 +1,32 @@
+package raft
+
+type Index uint64
+
+type Term uint64
+
+func (t Term) IsVotable(candidateTerm Term) bool {
+	return t <= candidateTerm
+}
+
+type Log struct {
+	Term  Term
+	Index Index
+	Data  []byte
+}
+
+type Logs []*Log
+
+func (l Logs) IsMoreUpToDate(candidateTerm Term, candidateIndex Index) bool {
+	if len(l) == 0 {
+		return false
+	}
+
+	lastLog := l[len(l)-1]
+	if lastLog.Term < candidateTerm {
+		return false
+	}
+	if lastLog.Term == candidateTerm && lastLog.Index <= candidateIndex {
+		return false
+	}
+	return true
+}
