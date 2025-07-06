a44a2f8e585f820b9c4022fd3c5bdbe2bc48bd7c
Jun Nishimura <n.junjun0303@gmail.com>
2025-07-06 16:44:38 +0900

    feat: add append method to log

--- raft/log.go
+++ raft/log.go
@@ -1,5 +1,11 @@
 package raft
 
+import (
+	"log/slog"
+
+	raftpb "github.com/JunNishimura/graft/raft/grpc"
+)
+
 type Index uint64
 
 type Term uint64
@@ -90,4 +96,65 @@ func (l Logs) DeleteAllAfter(index Index) Logs {
 	}
 
 	return l
+}
+
+func (l Logs) Append(logs ...*Log) Logs {
+	if len(logs) == 0 {
+		return l
+	}
+
+	if len(l) == 0 {
+		return logs
+	}
+
+	lastIndex := l.LastIndex()
+	for i, log := range logs {
+		if log.Index != lastIndex+Index(i+1) {
+			slog.Warn("logs are not sequential", "expected_index", lastIndex+Index(i+1), "actual_index", log.Index)
+		}
+
+		l = append(l, log)
+	}
+
+	return l
+}
+
+func convertToLogs(data map[string]interface{}, term Term, lastIndex Index) Logs {
+	logs := make(Logs, 0, len(data))
+	count := Index(0)
+	for key, value := range data {
+		count++
+
+		intValue, ok := value.(uint64)
+		if !ok {
+			// If the value is not a uint64, we skip it.
+			slog.Warn("skipping non-uint64 value in log conversion", "key", key, "value", value)
+			continue
+		}
+
+		logs = append(logs, &Log{
+			Term:  term,
+			Index: lastIndex + count,
+			Data: &LogData{
+				Key:   key,
+				Value: intValue,
+			},
+		})
+	}
+	return logs
+}
+
+func (l Logs) convertToLogEntries() []*raftpb.Entry {
+	entries := make([]*raftpb.Entry, 0, len(l))
+	for _, log := range l {
+		entries = append(entries, &raftpb.Entry{
+			Term:  uint64(log.Term),
+			Index: uint64(log.Index),
+			Data: &raftpb.EntryData{
+				Key:   log.Data.Key,
+				Value: log.Data.Value,
+			},
+		})
+	}
+	return entries
 }
