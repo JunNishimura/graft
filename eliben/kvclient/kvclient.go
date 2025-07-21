7f35cf53aa16149d4ebfba42759524d1591bc3d3
Jun Nishimura <n.junjun0303@gmail.com>
2025-07-21 11:07:08 +0900

    feat: add kvclient

---
+++ eliben/kvclient/kvclient.go
@@ -0,0 +1,161 @@
+package kvclient
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+
+	"github.com/JunNishimura/graft/eliben/api"
+)
+
+type KVClient struct {
+	addrs []string
+
+	assumedLeader int
+
+	clientID int32
+}
+
+func New(serviceAddrs []string) *KVClient {
+	return &KVClient{
+		addrs:         serviceAddrs,
+		assumedLeader: 0,
+		clientID:      clientCount.Add(1),
+	}
+}
+
+var clientCount atomic.Int32
+
+func (c *KVClient) Put(ctx context.Context, key, value string) (string, bool, error) {
+	putReq := api.PutRequest{
+		Key:   key,
+		Value: value,
+	}
+	var putResp api.PutResponse
+	err := c.send(ctx, "put", putReq, &putResp)
+	return putResp.PrevValue, putResp.KeyFound, err
+}
+
+func (c *KVClient) Get(ctx context.Context, key string) (string, bool, error) {
+	getReq := api.GetRequest{
+		Key: key,
+	}
+	var getResp api.GetResponse
+	err := c.send(ctx, "get", getReq, &getResp)
+	return getResp.Value, getResp.KeyFound, err
+}
+
+func (c *KVClient) CAS(ctx context.Context, key, compare, value string) (string, bool, error) {
+	casReq := api.CASRequest{
+		Key:          key,
+		CompareValue: compare,
+		Value:        value,
+	}
+	var casResp api.CASResponse
+	err := c.send(ctx, "cas", casReq, &casResp)
+	return casResp.PrevValue, casResp.KeyFound, err
+}
+
+func (c *KVClient) send(ctx context.Context, route string, req any, resp api.Response) error {
+FindLeader:
+	for {
+		retryCtx, retryCtxCancel := context.WithTimeout(ctx, 50*time.Millisecond)
+		path := fmt.Sprintf("http://%s/%s", c.addrs[c.assumedLeader], route)
+
+		c.clientlog("sending %#v to %v", req, path)
+		if err := sendJSONRequest(retryCtx, path, req, resp); err != nil {
+			if contextDone(ctx) {
+				c.clientlog("parent context done; bailing out")
+				retryCtxCancel()
+				return err
+			} else if contextDeadlineExceeded(retryCtx) {
+				c.clientlog("timed out: will try next address")
+				c.assumedLeader = (c.assumedLeader + 1) % len(c.addrs)
+				retryCtxCancel()
+				continue FindLeader
+			}
+			retryCtxCancel()
+			return err
+		}
+		c.clientlog("received response %#v", resp)
+
+		switch resp.Status() {
+		case api.StatusNotLeader:
+			c.clientlog("not leader; trying next address")
+			c.assumedLeader = (c.assumedLeader + 1) % len(c.addrs)
+			retryCtxCancel()
+			continue FindLeader
+		case api.StatusOK:
+			retryCtxCancel()
+			return nil
+		case api.StatusFailedCommit:
+			retryCtxCancel()
+			return fmt.Errorf("commit failed; please retry")
+		default:
+			panic("unreachable")
+		}
+	}
+}
+
+const DebugClient = 1
+
+func (c *KVClient) clientlog(format string, args ...any) {
+	if DebugClient > 0 {
+		clientName := fmt.Sprintf("[client%03d]", c.clientID)
+		format = clientName + " " + format
+		log.Printf(format, args...)
+	}
+}
+
+func sendJSONRequest(ctx context.Context, path string, reqData, respData any) error {
+	body := new(bytes.Buffer)
+	enc := json.NewEncoder(body)
+	if err := enc.Encode(reqData); err != nil {
+		return fmt.Errorf("JSON-encoding request data: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, path, body)
+	if err != nil {
+		return fmt.Errorf("creating HTTP request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("sending HTTP request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(respData); err != nil {
+		return fmt.Errorf("JSON-decoding response data: %w", err)
+	}
+
+	return nil
+}
+
+func contextDone(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+func contextDeadlineExceeded(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			return true
+		}
+	default:
+	}
+	return false
+}
